ebb6d17ffb42aebc41f8322b3b8a2df61e47cb3f
rifat-repliq <rifat.shikder@repliq.dev>
2023-09-11 12:28:54 +0600

    Added eslint for linting

--- app/index.jsx
+++ app/index.jsx
@@ -2,8 +2,8 @@ import { View, Text } from "react-native";
 
 export default function Page() {
   return (
-    <View>
-      <Text className="text-red-500">Tailwind integrated</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className=" text-teal-600 font-bold text-xl">CardiCheck</Text>
     </View>
   );
 }
